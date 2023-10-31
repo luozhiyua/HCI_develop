6c317de0995ee5239a3ded660db78d74a84e07b6
lzy <1295690991@qq.com>
2023-10-31 15:50:07 +0800

    test

--- src/main/java/com/example/travelre/pojo/entity/CityEntity.java
+++ src/main/java/com/example/travelre/pojo/entity/CityEntity.java
@@ -1,32 +1,31 @@
-package com.example.travelre.pojo.entity;
-
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.experimental.Accessors;
-
-
-@Entity
-@Table
-@Data
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
-@Accessors(chain = true)
-public class CityEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    private String cityName;
-
-
-    private String cityUrl1;
-    private String cityUrl2;
-    private String cityUrl3;
-
-    private String cityDetail;
-
-}
+//package com.example.travelre.pojo.entity;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Builder;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//import lombok.experimental.Accessors;
+//
+//
+//@Entity
+//@Table
+//@Data
+//@Builder
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@Accessors(chain = true)
+//public class CityEntity {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//
+//    private String cityName;
+//
+//
+//    private String cityUrl1;
+//    private String cityUrl2;
+//    private String cityUrl3;
+//
+//    private String cityDetail;
+//
+//}
--- src/main/java/com/example/travelre/pojo/entity/SceneEntity.java
+++ src/main/java/com/example/travelre/pojo/entity/SceneEntity.java
@@ -1,41 +1,41 @@
-package com.example.travelre.pojo.entity;
-
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.experimental.Accessors;
-
-
-@Entity
-@Table
-@Data
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
-@Accessors(chain = true)
-public class SceneEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    private String sceneName;
-
-    private String sceneAddr;
-
-    private String sceneIntro;
-
-    private double sceneScore;
-
-    private String sceneUrl1;
-    private String sceneUrl2;
-    private String sceneUrl3;
-
-    private String sceneDetail;
-
-//    @ForeignKey()
-    private Long cityID;
-
-
-}
+//package com.example.travelre.pojo.entity;
+//
+//import jakarta.persistence.*;
+//import lombok.AllArgsConstructor;
+//import lombok.Builder;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//import lombok.experimental.Accessors;
+//
+//
+//@Entity
+//@Table
+//@Data
+//@Builder
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@Accessors(chain = true)
+//public class SceneEntity {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//
+//    private String sceneName;
+//
+//    private String sceneAddr;
+//
+//    private String sceneIntro;
+//
+//    private double sceneScore;
+//
+//    private String sceneUrl1;
+//    private String sceneUrl2;
+//    private String sceneUrl3;
+//
+//    private String sceneDetail;
+//
+////    @ForeignKey()
+//    private Long cityID;
+//
+//
+//}
